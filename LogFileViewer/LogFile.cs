e6d699c7a40a5be3f91568372f1310b6fcab99f0
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2023-02-10 17:06:36 +0300

    Start

---
+++ LogFileViewer/LogFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogFileViewer
+{
+    public class LogFile
+    {
+        public string filePath { get; set; }
+        public string logData { get; set; }
+        private DateTime LastWriteTime;
+
+        public LogFile (string filePath)
+        {
+            this.filePath = filePath;
+            Update();
+        }
+
+        public bool isModified()
+        {
+            if (File.GetLastWriteTime(this.filePath) > LastWriteTime)
+                return true;
+            return false;
+        }
+
+        public void Update()
+        {
+            logData = File.ReadAllText(this.filePath);
+            LastWriteTime = File.GetLastWriteTime(this.filePath);
+        }
+    }
+}
---
+++ LogFileViewer/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LogFileViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        LogFile logFile;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+
+            logFile = new LogFile(@"test.txt");
+
+            Update();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (logFile != null)
+            {
+                if (logFile.isModified())
+                {
+                    logFile.Update();
+                    Update();
+                }
+            }
+        }
+
+        private void Update()
+        {
+            textBox.Text = logFile.logData;
+        }
+
+        private void LabelSettings_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("111");
+        }
+
+        private void LabelOpen_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Открыть файл");
+        }
+    }
+}
