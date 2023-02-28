83bbc3b873608df3a2871f1e3e31e91ff09b4f7f
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2023-02-28 13:51:19 +0300

    Update

--- LogFileViewer/LogFile.cs
+++ LogFileViewer/LogFile.cs
@@ -31,39 +31,8 @@ namespace LogFileViewer
 
         public void Update()
         {
-            logData = File.ReadAllText(this.filePath);
-
-            //Encoding cp1251 = Encoding.GetEncoding(1251);
-            //Encoding unicode = Encoding.Unicode;
-
-            //byte[] cp1251Bytes = cp1251.GetBytes(logData);
-            //byte[] unicodeBytes = Encoding.Convert(cp1251, unicode, cp1251Bytes);
-
-            //char[] asciiChars = new char[unicode.GetCharCount(unicodeBytes, 0, unicodeBytes.Length)];
-            //unicode.GetChars(unicodeBytes, 0, unicodeBytes.Length, asciiChars, 0);
-            //string asciiString = new string(asciiChars);
-
-            //char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
-            //ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
-            //string asciiString = new string(asciiChars);
-
-
-            //logData = asciiString;
-
-            logData = Win1251ToUTF8(logData);
-
+            logData = File.ReadAllText(this.filePath, Encoding.GetEncoding(1251));
             LastWriteTime = File.GetLastWriteTime(this.filePath);
-        }
-
-        static private string Win1251ToUTF8(string source)
-        {
-            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            Encoding utf8 = Encoding.UTF8;
-            Encoding win1251 = CodePagesEncodingProvider.Instance.GetEncoding(1251);
-            byte[] utf8Bytes = win1251.GetBytes(source);
-            byte[] win1251Bytes = Encoding.Convert(win1251, utf8, utf8Bytes);
-            source = win1251.GetString(win1251Bytes);
-            return source;
         }
     }
 }
